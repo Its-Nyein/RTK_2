1f5a3c440ab62e4446e0f60795cd467729978dad
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-22 03:22:56 +0630

    modify reaction data and ReactionsButton component

--- src/features/posts/postSlice.jsx
+++ src/features/posts/postSlice.jsx
@@ -69,6 +69,6 @@ const postSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postSlice.actions;
+export const { postAdded, reactionsAdded } = postSlice.actions;
 
 export default postSlice.reducer;
