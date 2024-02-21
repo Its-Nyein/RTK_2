862a39f23124dedc5cff44882aea2eeb8264a47d
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-21 17:53:30 +0630

    modify the AddPostForm component

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 // nanoid generate random id so we don't need
 // to import something else like uuid
 import { postAdded } from "./postSlice";
+import { selectAllUsers } from "../users/userSlice";
 
 function AddPostForm() {
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
 
   const handleOnSavePost = () => {
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
       setTitle("");
       setContent("");
     }
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const users = useSelector(selectAllUsers);
+  const userOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
@@ -31,15 +41,26 @@ function AddPostForm() {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+
+        <label htmlFor="postAuthor">Author:</label>
+        <select
+          id="postAuthor"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        >
+          <option value=""></option>
+          {userOptions}
+        </select>
+
         <label htmlFor="postContent">Content:</label>
-        <input
+        <textarea
           type="text"
           name="postContent"
           id="postContent"
           value={content}
           onChange={(e) => setContent(e.target.value)}
         />
-        <button type="button" onClick={handleOnSavePost}>
+        <button type="button" onClick={handleOnSavePost} disabled={!canSave}>
           Save Post
         </button>
       </form>
