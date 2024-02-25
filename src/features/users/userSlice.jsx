a73c5225486f964920ce1c07ff7bcaf1551c751d
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-02-26 06:17:37 +0630

    creating a fetchUsers async thunk

--- src/features/users/userSlice.jsx
+++ src/features/users/userSlice.jsx
@@ -1,15 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "Dude Lebowski" },
-  { id: "1", name: "Neil Young" },
-  { id: "2", name: "Nyein Phyo" },
-];
+const USERS_URL = "https://jsonplaceholder.typicode.com/posts";
+
+export const fetchUsers = createAsyncThunk("posts/userPosts", async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
+
+const initialState = [];
 
 const userSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
