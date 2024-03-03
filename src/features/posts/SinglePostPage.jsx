b91e7ed322990f2cfe6e43f61b58652186b89893
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-03-04 05:28:10 +0630

    update the SinglePostPage

---
+++ src/features/posts/SinglePostPage.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postSlice";
+
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionsButton from "./ReactionsButton";
+
+import React from "react";
+
+function SinglePostPage() {
+  const post = useSelector((state) => selectPostById(state, postId));
+
+  if (!post) {
+    <section>
+      <h2>Page not found!</h2>
+    </section>;
+  }
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionsButton post={post} />
+    </article>
+  );
+}
+
+export default SinglePostPage;
