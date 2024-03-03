302ca1fffb07c89d8c65592ab6859c06596e2d9e
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-03-04 05:30:33 +0630

    importing and applying react router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
 import { fetchUsers } from "./features/users/userSlice.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 store.dispatch(fetchUsers());
 
@@ -13,7 +14,11 @@ store.dispatch(fetchUsers());
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
