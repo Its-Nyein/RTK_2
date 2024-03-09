9bc255860480c5c06a0383cf29d23ec4d8d14b9e
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-03-09 11:22:28 +0630

    adding count state, reducer and selector

--- src/features/posts/postSlice.jsx
+++ src/features/posts/postSlice.jsx
@@ -9,6 +9,7 @@ const initialState = {
   status: "idle",
   // 'loading' | 'succeeded' | 'failed',
   error: null,
+  count: 0,
 };
 
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
@@ -64,35 +65,15 @@ const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded: {
-      reducer(state, action) {
-        state.posts.push(action.payload);
-      },
-      prepare(title, content, userId) {
-        return {
-          payload: {
-            id: nanoid(),
-            title,
-            content,
-            date: new Date().toISOString(),
-            userId,
-            reactions: {
-              thumbsUp: 0,
-              wow: 0,
-              heart: 0,
-              rocket: 0,
-              coffee: 0,
-            },
-          },
-        };
-      },
-    },
     reactionsAdded(state, action) {
       const { postId, reaction } = action.payload;
       const currentPost = state.posts.find((post) => post.id === postId);
       if (currentPost) {
         currentPost.reactions[reaction]++;
       }
+    },
+    increaseCount(state, action) {
+      state.count = state.count + 1;
     },
   },
   extraReducers(builder) {
@@ -170,6 +151,7 @@ const postSlice = createSlice({
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
+export const getCount = (state) => state.posts.count;
 
 export const selectPostById = (state, postId) =>
   state.posts.posts.find((post) => post.id === postId);
