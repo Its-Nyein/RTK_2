fc39c5fe3fe86087b2743259e7507e99303c47cf
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-03-14 18:32:07 +0630

    export the auto-generated hooks

--- src/features/posts/postSlice.jsx
+++ src/features/posts/postSlice.jsx
@@ -190,7 +190,13 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
 //   },
 // });
 
-export const { useGetPostsQuery, useGetPostsByUserIdQuery } = extendedApiSlice;
+export const {
+  useGetPostsQuery,
+  useGetPostsByUserIdQuery,
+  useAddNewPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = extendedApiSlice;
 
 // returns the query result object
 export const selectPostsResult = extendedApiSlice.endpoints.getPosts.select();
