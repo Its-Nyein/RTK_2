a6c813e87a99562575ece99a65fce730e9f86cac
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-03-14 17:38:59 +0630

    Link to the PostAuthor component

--- src/features/posts/PostAuthor.jsx
+++ src/features/posts/PostAuthor.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/userSlice";
+import { Link } from "react-router-dom";
 
 import React from "react";
 
@@ -7,7 +8,16 @@ function PostAuthor({ userId }) {
   const users = useSelector(selectAllUsers);
 
   const author = users.find((user) => user.id === userId);
-  return <span>by {author ? author.name : "Unknown Author"}</span>;
+  return (
+    <span>
+      by{" "}
+      {author ? (
+        <Link to={`/user/${userId}`}>{author.name}</Link>
+      ) : (
+        "Unknown Author"
+      )}
+    </span>
+  );
 }
 
 export default PostAuthor;
